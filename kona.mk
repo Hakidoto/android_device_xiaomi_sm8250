c0dbc28c7608f0f75e0bdf32c1ebbbe8fd4399a9
Hadid Garcia <hadid.garcia1@gmail.com>
2021-09-20 19:41:22 -0500

    sm8250-common: remove snap camera package
    ugh :>
    
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- kona.mk
+++ kona.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor\
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    Snap \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Charger
