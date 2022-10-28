f22b7af6bc2d404520e69854244e7e479f49210b
Hadid Garcia <hadid.garcia1@gmail.com>
2022-10-28 17:31:12 -0500

    sm8250-common: fix compilation - Add clang path - remove test exclusion

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,6 +139,7 @@ TARGET_KERNEL_ARCH := arm64
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := zyc
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/zyc-clang-14
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 LLVM_IAS=1
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
